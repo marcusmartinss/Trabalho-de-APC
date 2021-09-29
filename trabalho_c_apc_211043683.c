c6598545cf98590c5973ea7fa30832e509641021
Marcus Martins <vini.mark2002@gmail.com>
2021-09-29 15:14:39 -0300

    Create trabalho_c_apc_211043683.c

---
+++ trabalho_c_apc_211043683.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+
+int menu(int repeat)
+{
+    int num_menu;
+
+    do
+    {
+        printf("     ______________________________________\n");
+        printf("    |            Galactic Birds            |\n");
+        printf("    |    'Sua confianca sob nossas asas'   |\n");
+        printf("    |--------------------------------------|\n");
+        printf("    | Menu de reservas espaciais           |\n");
+        printf("    |--------------------------------------|\n");
+        printf("    | 1) Cadastrar voo.                    |\n");
+        printf("    | 2) Cadastrar reserva.                |\n");
+        printf("    | 3) Consulta voo.                     |\n");
+        printf("    | 4) Consultar reserva.                |\n");
+        printf("    | 5) Consultar passageiro.             |\n");
+        printf("    | 6) Cancelar voo.                     |\n");
+        printf("    | 7) Cancelar reserva.                 |\n");
+        printf("    | 8) Excluir voo.                      |\n");
+        printf("    | 9) Sair do programa.                 |\n");
+        printf("    |______________________________________|\n\n");
+
+        printf("           Qual menu deseja acessar?\n");   
+        printf("                      "); scanf("%d", &num_menu);
+
+        switch (num_menu)
+        {
+            case 1:
+                repeat = 0;
+                return num_menu;
+                break;
+            case 2: 
+                repeat = 0;
+                return num_menu;
+                break;
+            case 3:
+                repeat = 0;
+                return num_menu;
+                break;
+            case 4:
+                repeat = 0;
+                return num_menu;
+                break;
+            case 5:
+                repeat = 0;
+                return num_menu;
+                break;
+            case 6:
+                repeat = 0;
+                return num_menu;
+                break;
+            case 7:
+                repeat = 0;
+                return num_menu;
+                break;
+            case 8:
+                repeat = 0;
+                return num_menu;
+                break;
+            case 9:
+                repeat = 0;
+                return num_menu;
+                break;
+            default:
+                printf("\n\n            ERRO: Valor invalido!\n\n");
+                repeat = 1;
+        }
+
+    } while (repeat == 1);
+}
+
+int cadastrar_voo()
+{
+ return 0;
+}
+
+int cadastrar_reserva()
+{
+ return 0;
+}
+
+int consultar_voo()
+{
+ return 0;
+}
+
+int consultar_reserva()
+{
+ return 0;
+}
+
+int consultar_passageiro()
+{
+ return 0;
+}
+
+int cancelar_voo()
+{
+ return 0;
+}
+
+int cancelar_reserva()
+{
+ return 0;
+}
+
+int excluir_voo()
+{
+ return 0;
+}
+
+int main()
+{
+    FILE *teste;
+    
+    int num_menu, repeat = 1;
+
+    do
+    {
+        num_menu = menu(repeat);
+
+        if ( num_menu == 9)
+        {
+            repeat =  0;
+        }
+        else
+        {
+            switch(num_menu)
+            {
+                case 1:
+                    cadastrar_voo();
+                    break;
+                case 2:
+                    cadastrar_reserva();
+                    break;
+                case 3:
+                    consultar_voo();
+                    break;
+                case 4:
+                    consultar_reserva();
+                    break;
+                case 5:
+                    consultar_passageiro();
+                    break;
+                case 6:
+                    cancelar_voo();
+                    break;
+                case 7:
+                    cancelar_reserva();
+                    break;
+                case 8:
+                    excluir_voo();
+                    break;
+            }
+        }
+    }
+    while(repeat == 1);
+
+ return 0;
+}
